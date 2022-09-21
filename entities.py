04fbb88f0827ce0726cba51fb3a617adb2d8373a
vanshitau <vanshitauthra@gmail.com>
2022-09-20 23:24:27 -0400

    adding booking entity

--- entities.py
+++ entities.py
@@ -20,3 +20,34 @@ class Reviews:
     # setter method for comments
     def set_comments(self, x):
         self.comment = x
+
+class Bookings:
+    # initializing components of reviews entity
+    def __init__(self, verification = "", date = 0, duration = ""):
+         self.verification = verification
+         self.date = date
+         self.duration = duration
+      
+    # getter method for verification
+    def get_verfication(self):
+        return self.verification
+      
+    # setter method for verification
+    def set_verification(self, x):
+        self.verification = x
+
+    # getter method for date
+    def get_date(self):
+        return self.date
+
+    # setter method for date
+    def set_date(self, x):
+        self.date = x
+
+     # getter method for duration
+    def get_duration(self):
+        return self.duration
+
+    # setter method for date
+    def set_duration(self, x):
+        self.duration = x
