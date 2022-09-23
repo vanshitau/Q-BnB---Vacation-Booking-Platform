ee033867158efebc0e20d90abed4e55e5ebb0499
vanshitau <vanshitauthra@gmail.com>
2022-09-23 16:59:31 -0400

    changes made to the booking class

--- entities.py
+++ entities.py
@@ -23,31 +23,40 @@ class Reviews:
 
 class Bookings:
     # initializing components of reviews entity
-    def __init__(self, verification = "", date = 0, duration = ""):
+    def __init__(self, verification = "", date = "", duration = ""):
          self.verification = verification
          self.date = date
          self.duration = duration
+         self.is_paid = is_paid
       
     # getter method for verification
     def get_verfication(self):
         return self.verification
       
     # setter method for verification
-    def set_verification(self, x):
-        self.verification = x
+    def set_verification(self, verify):
+        self.verification = verify
 
     # getter method for date
     def get_date(self):
         return self.date
 
     # setter method for date
-    def set_date(self, x):
-        self.date = x
+    def set_date(self, date):
+        self.date = date
 
      # getter method for duration
     def get_duration(self):
         return self.duration
 
-    # setter method for date
-    def set_duration(self, x):
-        self.duration = x
+    # setter method for duration
+    def set_duration(self, duration):
+        self.duration = duration
+
+    # getter method for paid
+    def get_is_paid(self):
+        return self.is_paid
+
+    # setter method for paid
+    def set_is_paid(self, paid):
+        self.is_paid = paid
