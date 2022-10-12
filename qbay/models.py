c90809d3c16ad19f2ed9dae622110cf552f46136
Bilal Imran <bilalimran@Bilals-MacBook-Pro.local>
2022-10-11 20:12:30 -0400

    created functions for the create listing function

--- qbay/models.py
+++ qbay/models.py
@@ -1,5 +1,7 @@
+from sqlalchemy import PrimaryKeyConstraint
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
+#from entities import *
 
 
 '''
@@ -18,16 +20,31 @@ class User(db.Model):
         primary_key=True)
     password = db.Column(
         db.String(120), nullable=False)
+    owner_id = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Listing(db.Model):
+    id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(80), nullable=False, primary_key=True)
+    description = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    last_modified_date = db.Column(db.String(80), nullable=False)
+    owner_id = db.Column(db.Integer, nullable=False)
+    
+
+    def __repr__(self):
+        return '<User %r>' % self.title
+
+
 
 
 # create all tables
 db.create_all()
 
 
-def register(name, email, password):
+def register(owner_id,name, email, password):
     '''
     Register a new user
       Parameters:
@@ -43,7 +60,7 @@ def register(name, email, password):
         return False
 
     # create a new user
-    user = User(username=name, email=email, password=password)
+    user = User(owner_id=owner_id,username=name, email=email, password=password)
     # add it to the current database session
     db.session.add(user)
     # actually save the user object
@@ -65,3 +82,100 @@ def login(email, password):
     if len(valids) != 1:
         return None
     return valids[0]
+
+def title_desc(id,title,description,last_modified_date, price,owner_id):
+    '''
+    Check the title and description
+      Parameters:
+        id (string):     listing id
+        title (string):    listing title
+        description (string): user password
+        last_modified_date:   listing last modified date
+        price:                listing price
+        owner_id:             owner id of the listing
+      Returns:
+        True if title and description meet the requirements otherwise False
+    '''
+    #fix - check to see if all characters are alphanum
+    #this does not check to see if all the characters are alphanumeric - to do this you would need to use regex
+    # and check to see if each character is a-z A-z or 0-9
+
+    #check to see if the title exists
+    existed = Listing.query.filter_by(title=title).first()
+
+    #if the title is not an empty sting, check to see if the first and last char are not spaces
+    #make sure the length of the description is more than the title length and is <2000 and >20
+    if not(existed):
+        if (len(title) > 0):
+            if (title[0] != " ") and (len(title) <= 80) and  title[-1] != " " and len(description) > 20 and len(description)< 2000 and (len(description) > len(title)) :
+                return True
+
+    # create a new user
+    listing = Listing(id=id, title=title, description=description,last_modified_date=last_modified_date, price=price,owner_id=owner_id)
+    # add it to the current database session
+    db.session.add(listing)
+    # actually save the user object
+    db.session.commit()
+
+    return False
+    
+def check_price(price):
+    '''
+    Check the title and description
+      Parameters:
+        price:                listing price
+      Returns:
+        True if the price meets the requirements otherwise False
+    '''
+    if(10<=price<=10000):
+        return price
+
+def check_date(date_modified):
+    '''
+    Check the title and description
+      Parameters:
+        date_modified:                listing date
+      Returns:
+        True if the date modified meets the requirements otherwise False
+    '''
+    if ('2021-01-02' <= date_modified <= '2025-01-02'):
+        return date_modified
+
+def check_owner(email, owner_id):
+    '''
+    Check the title and description
+      Parameters:
+        email:                the owner's email of the listing 
+        owneer_id:            the owner's id of the listing 
+      Returns:
+        True if the email  meets the requirements and the user is in the db otherwise False
+    '''
+    
+
+    #check the database to find the user's email
+    user = User.query.filter_by(email=owner_id).first()
+   # listing_email = user.email
+    #the owner does exist in the db
+    if user:
+        #the email is not empty
+        if email != '':
+            return True
+    else:
+        return False
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+
+
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,14 +1,13 @@
-from qbay.models import register, login
-
+from qbay.models import register, login, check_price, check_date, title_desc, check_owner
 
 def test_r1_7_user_register():
     '''
     Testing R1-7: If the email has been used, the operation failed.
     '''
 
-    assert register('u0', 'test0@test.com', '123456') is True
-    assert register('u0', 'test1@test.com', '123456') is True
-    assert register('u1', 'test0@test.com', '123456') is False
+    assert register(1, 'u0', 'test0@test.com', '123456') is True
+    assert register( 2, 'u0', 'test1@test.com', '123456') is True
+    assert register( 3, 'u1', 'test0@test.com', '123456') is False
 
 
 def test_r2_1_login():
@@ -25,3 +24,50 @@ def test_r2_1_login():
 
     user = login('test0@test.com', 1234567)
     assert user is None
+
+def test_r4_1_to_4_title():
+  '''
+  Testing R4-1 and R4-2: A user can create a listing only if the title is alphanumeric and 
+  if the title does not begin with a space
+  '''
+  assert title_desc(1,"Condo for rent","Two Bedroom house located in center of downtown Toronto", 1000, '2022-01-02',123456) is True
+  assert title_desc(1,"Apartment for rent","Two Bedroom house located in center of downtown Toronto", 1000, '2022-01-02',123456) is True
+  assert title_desc(1,"Condo for rent","Two", 1000, '2022-01-02',123456) is False
+
+
+def test_r4_5_price():
+  '''
+  Testing R4-5: The price of the listing must between 10 and 10,000
+  '''
+  price = check_price(2000)
+  assert price is not None
+
+  price = check_price(2)
+  assert price is None
+  
+def test_r4_6_date():
+  '''
+  Testing R4-6: The date of the listing must between 2021-01-02' and '2025-01-02
+  '''
+  date = check_date('2024-01-05')
+  assert date is not None
+
+  date = check_date('2019-01-05')
+  assert date is None
+
+def test_r4_7_owner():
+  '''
+  Testing R4-7: This will check to see if the user's email is not empty and make sure the user exists in the db
+  '''
+  #should give false since there is no owner in the database with that id
+  assert check_owner('test0@test.com', 202019) is False
+  #should give true since there is an owner in the database with that id
+  assert check_owner('test0@test.com', 202019) is True
+
+ 
+  
+
+   
+    
+
+  
