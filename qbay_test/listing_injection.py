c3515caa2e747687d04054749b15cb96b5c1757e
Keenan Taylor <keenantaylor@Keenans-MacBook-Pro.local>
2022-11-16 16:51:51 -0500

    added failing test cases

--- qbay_test/listing_injection.py
+++ qbay_test/listing_injection.py
@@ -1,14 +1,29 @@
+from qbay.models import (listing, register)
+from datetime import datetime
 
-file1 = open('Generic_SQL.txt', 'w')
-file1.writelines(L)
-file1.close()
-  
 # Using readlines()
-file1 = open('myfile.txt', 'r')
+file1 = open('Generic_SQLI.txt', 'r')
 Lines = file1.readlines()
-  
-count = 0
-# Strips the newline character
-for line in Lines:
-    count += 1
-    print("Line{}: {}".format(count, line.strip()))
+
+user = register(None, 'user0', 'test@test.com', 'Abcdef!')
+
+def test_sql_listing_title():
+    for line in Lines:
+        listing1 = listing(None, line, "My house this is a description", 100, user.id,
+            datetime(2024, 1, 5).strftime('%Y-%m-%d')
+        )
+        assert listing1 is not None
+        
+def test_sql_listing_description():
+    for line in Lines:
+        listing2 = listing(None, "title", line, 100, user.id,
+            datetime(2024, 1, 5).strftime('%Y-%m-%d')
+        )   
+        assert listing2 is not None
+
+def test_sql_listing_price():
+    for line in Lines:
+        listing3 = listing(None, "title", "My house this is a description", line, user.id,
+            datetime(2024, 1, 5).strftime('%Y-%m-%d')
+        )   
+        assert listing3 is not None
