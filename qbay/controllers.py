28b95df99111c147cbebbf11a9a545449a2cccea
Amanda Misek <amandamisek@Amandas-MacBook-Pro.local>
2022-12-05 21:46:25 -0500

    changes from comments

--- qbay/controllers.py
+++ qbay/controllers.py
@@ -29,9 +29,7 @@ def authenticate(inner_function):
         print("session: ", session)
         if 'logged_in' in session:
             email = session['logged_in']
-            # print("logged in: ", email)
             try:
-                # print("email", email)
                 user = User.query.filter_by(email=email).one_or_none()
                 
                 if user:
@@ -43,11 +41,8 @@ def authenticate(inner_function):
                 print(e)
                 pass
         else:
-            # else, redirect to the login page
             print("loggin in")
             return redirect('/login')
-    # print("returnning wrapped inner")
-    # return the wrapped version of the inner_function:
     return wrapped_inner
 
 
@@ -90,7 +85,6 @@ def home(user):
     # the login checking code all the time for other
     # front-end portals
     # the user's listings
-    # all_listings = Listing.query.filter_by(owner_id=user.id).all()
     all_listings = Listing.query.all()
     if len(all_listings) > 0:
         listings = []
@@ -258,7 +252,7 @@ def update_get():
 
 @app.route('/updateUser', methods=['POST'])
 def update_post():
-    # getting info from form
+    # getting info
     username = request.form.get('username')
     email_new = request.form.get('email')
     billing_address = request.form.get('billing_address')
@@ -267,17 +261,14 @@ def update_post():
     email = session['logged_in']
     user = User.query.filter_by(email=email).one_or_none()
 
-    # calling update functionx
+    # calling update function
     user = update_user(
         user.id, username, email_new, billing_address, postal_code
     )
     if user:
-        # session['updated_user'] = user.email
-        # are we supposed to use success? If so how?
         success = user
         if not success:
             error_message = "Update failed."
-        # logic not quite right here
         if error_message:
             return render_template('updateUser.html', message=error_message)
         else:
