3d1e23dcfb94b59870e8cab73956294b5d382248
keenan78 <95659224+keenan78@users.noreply.github.com>
2022-12-05 21:41:40 -0500

    Update qbay/models.py
    Co-authored-by: Amanda Misek <74020381+manda-02@users.noreply.github.com>

--- qbay/models.py
+++ qbay/models.py
@@ -585,7 +585,6 @@ def booked(listing_id, buyer_id, booked_start_date, booked_end_date):
         db.session.add(booking)
         db.session.commit()
         user.account_bal = user.account_bal - listing.price
-        print("commit to db")
         return True
 
     # checks to see if the listing is the users listing
